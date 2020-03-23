04e6e8151296ba1fa6f6d8f1383a8fb391f6d80a
Sheila <sbonilla@excelia-bpm.com>
2020-03-23 17:02:20 +0100

    Ejemplo Node-Redis

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+
+const fetch = require('node-fetch');
+
+const redis  = require('redis');
+
+const PORT = process.env.PORT || 5000;
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+
+const client = redis.createClient(REDIS_PORT); 
+
+const app = express();
+
+// cache middleware
+
+function cache(req,res,next)
+{
+    const {username} = req.params;
+
+    client.get(username,(err,data) =>{
+
+        if(err) throw err;
+
+        if(data !== null)
+        {
+           res.send(setResponse(username,data));
+        }
+        else{
+            next();
+        }
+
+    });
+
+
+}
+
+app.get('/repos/:username',cache,getRepos);
+
+async function getRepos(req,res,next)
+{
+   try{
+
+    console.log("Fetching Data");
+
+    const {username} = req.params;
+
+    const response = await fetch(`https://api.github.com/users/${username}`);
+
+    const data = await response.json();
+
+    const repos = data.public_repos;
+
+    client.setex(username,3600,repos);
+
+    res.send(setResponse(username,repos));
+
+   }catch(err){
+    
+      console.log(err);
+
+   }
+}
+
+function setResponse(username,repos)
+{
+    return `<h2>${username} has ${repos} Github Repos</h2>`;
+}
+
+app.listen(5000,() =>{
+    console.log(`App listening on PORT ${PORT}`);
+})
